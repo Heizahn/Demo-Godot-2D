715930b59730837e13e78a75152c50f475699c3a
Heizahn <humbertorbw@gmail.com>
2023-11-22 14:56:33 -0400

    scene main complete

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -8,17 +8,39 @@ func _ready():
 	new_game()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func game_over():
-	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$ScoreTimer.stop()
 
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTime.start()
+
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate()
+
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+
+
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	mob.position = mob_spawn_location.position
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	var velocity = Vector2(randf_range(150.0, 350.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	add_child(mob)
+
+
+func _on_start_time_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_score_timer_timeout():
+	score += 1
